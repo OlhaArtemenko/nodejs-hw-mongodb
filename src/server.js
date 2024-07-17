22ab1c2a05e68b513f394110b3abb40057b944c8
Olha Art <global14072012@gmail.com>
2024-07-17 16:04:59 +0300

    f

--- src/server.js
+++ src/server.js
@@ -1,11 +1,9 @@
-// server.js
-
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino';
-import expressPino from 'pino-http';
+import pinoHttp from 'pino-http';
 import dotenv from 'dotenv';
-import contactsRouter from './routes/contacts.js';
+import contactsRouter from './routes/contacts.mjs';
 
 dotenv.config();
 
@@ -14,7 +12,8 @@ const logger = pino();
 const port = process.env.PORT || 3030;
 
 app.use(cors());
-app.use(expressPino({ logger }));
+app.use(pinoHttp({ logger }));
+
 app.use(express.json());
 
 app.use('/contacts', contactsRouter);
@@ -23,10 +22,8 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
 });
 
-function setupServer() {
+export function setupServer() {
   app.listen(port, () => {
     logger.info(`Server is running on port ${port}`);
   });
 }
-
-export { setupServer };
